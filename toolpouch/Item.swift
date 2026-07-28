1b946e12f3e9212f633451514f55b66a5cf667b4
hoxim <mr@hoxim.com>
2026-07-28 12:21:47 +0200

    Initial Commit

---
+++ toolpouch/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  toolpouch
+//
+//  Created by Marcin Ryzko on 28/07/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ toolpouchTests/toolpouchTests.swift
@@ -0,0 +1,18 @@
+//
+//  toolpouchTests.swift
+//  toolpouchTests
+//
+//  Created by Marcin Ryzko on 28/07/2026.
+//
+
+import Testing
+
+struct toolpouchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
